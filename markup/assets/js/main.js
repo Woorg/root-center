24cbdb3bd7239bdff69f13320520e3936e2b1f34
Igor Gorlov <gorlov35@gmail.com>
2019-07-31 05:49:35 +0300

    add responsive

--- markup/assets/js/main.js
+++ markup/assets/js/main.js
@@ -1,6 +1,8 @@
 import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
-// import bound from 'bounds.js';
+// import $ from 'jquery';
+// import slick from 'slick-carousel';
+// import mask from "jquery-mask-plugin";
+// import LazyLoad from "vanilla-lazyload";
 
 (function ($) {
 
@@ -21,9 +23,118 @@ import $ from 'jquery';
 
     console.log('%c developed by igor gorlov gorlov https://webjeb.ru', styles);
 
+    let lazyLoadInstance = new LazyLoad({
+        elements_selector: ".lazy"
+        // ... more custom settings?
+    });
 
-    $(function() {
+    if (lazyLoadInstance) {
+        lazyLoadInstance.update();
+    }
 
+
+    $(function () {
+
+        // Banner slider
+
+        let $photoSlider = $('.photo__slider');
+        let $photoThumbs = $('.photo__thumbs');
+
+
+        if ( $photoSlider.length > 0 ) {
+            $photoSlider.slick({
+                mobileFirst: true,
+                dots: false,
+                arrows: true,
+                // lazyLoad: 'ondemand',
+                infinite: true,
+                fade: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 300,
+                adaptiveHeight: true,
+                asNavFor: '.photo__thumbs'
+            });
+        }
+
+        if ( $photoThumbs.length > 0 ) {
+            $photoThumbs.slick({
+                mobileFirst: true,
+                dots: false,
+                // lazyLoad: 'ondemand',
+                arrows: false,
+                infinite: true,
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                focusOnSelect: true,
+                speed: 300,
+                centerMode: false,
+                centerPadding: '0',
+                adaptiveHeight: false,
+                asNavFor: '.photo__slider',
+                responsive: [{
+                    breakpoint: 577,
+                    settings: {
+                        slidesToShow: 4
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 6
+                    }
+                }]
+            });
+        }
+
+
+        /*
+            Scroll to element
+        */
+
+        $('.header__button, .footer__button').on( 'click', function(e) {
+            e.preventDefault();
+            let _scroll = $(this).attr('href');
+            if (_scroll != '#' && $(_scroll).length) {
+                $('html, body').animate({ scrollTop: $(_scroll).offset().top  }, 300);
+            }
+        });
+
+
+        /*
+         Phone Mask
+        */
+
+        $('.form__field_phone input').mask("+ 7(999)999-99-99", {
+            placeholder: "+7(___)___-__-__"
+        });
+
+
+        // Map
+
+        let myMap;
+
+        // Дождёмся загрузки API и готовности DOM.
+        ymaps.ready(init);
+
+        function init () {
+            // Создание экземпляра карты и его привязка к контейнеру с
+            // заданным id ("map").
+            myMap = new ymaps.Map('map', {
+                // При инициализации карты обязательно нужно указать
+                // её центр и коэффициент масштабирования.
+                center: [56.8519, 60.6122], // Москва
+                zoom: 10
+            }, {
+                searchControlProvider: ''
+            });
+
+            document.getElementById('destroyButton').onclick = function () {
+                // Для уничтожения используется метод destroy.
+                myMap.destroy();
+            };
+
+        }
 
 
     });
